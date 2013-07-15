970c52e6f4abc3bf7a7cbb4aad2c2454e82a5f92
miguelithoh <maherrera21@utpl.edu.ec>
2013-07-15 00:11:17 -0500

    Create Precio.java

---
+++ Precio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejm2;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Precio {
+    public double euros;
+    public double da(){return euros;}
+     
+    public void poner(double x){
+        euros = x;
+    }    
+    }
